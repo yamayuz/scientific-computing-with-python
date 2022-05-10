5e7f87c7d1f574d25f04a94f76237641a87d2056
yamayuz <yuki@yamayulab.com>
2022-05-10 23:51:02 +0900

    [update] newton_method

--- numeric_calc/newton_method.py
+++ numeric_calc/newton_method.py
@@ -1,12 +1,33 @@
 # ニュートン法により、2の平方根を求める
-x = 5.0
 
-while True:
-    x2 = x - (x**2 - 2) / (x * 2)
+def newton_method(x_ini, eps, max_iters=500):
+    x, x_old = x_ini, x_ini
+    iter_no, err = 0, 1
+    while (err > eps):
+        if x == 0:
+            print("zero division err")
+            break
+        else:
+            x = x - (x**2 - 2) / (2 * x)
+        err = abs(x - x_old)
+        print("iter: {:3d}, x: {:4f}, err: {:4f}".format(iter_no, x, err))
+        x_old = x
+        iter_no += 1
+        if iter_no >= max_iters:
+            break
+    return x
 
-    if abs(x2 - x) < 0.0001:
-        break
 
-    x = x2
+if __name__ == "__main__":
+    root2 = newton_method(5.0, 1e-4)
+    print(root2)
 
-print(x)
+    # x = 5.0
+    # while True:
+    #     x2 = x - (x**2 - 2) / (x * 2)
+
+    #     if abs(x2 - x) < 0.0001:
+    #         break
+
+    #     x = x2
+    # print(x)
